f8e18890c38cfa332d9ac851850088395cdae992
Takeshi Kimoto <lnuk2009jp@gmail.com>
2019-08-26 21:56:50 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -34,8 +34,20 @@ end
 
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
-    if (cart.keys.include?(coupon[:item]) && cart[coupon[:item][:count] >= coupon[:num])
+    if cart.keys.include?(coupon[:item]) && cart[coupon[:item][:count] >= coupon[:num]
       new_name = "#{coupon[:item]} W/COUPON"
+       if cart[new_name]
+         cart[new_name][:count] += coupon[:num]
+       else
+         cart[new_name] = {
+           count: coupon[:num],
+           price: coupon[:pirce]/cart[:num],
+           clearance: cart[coupon[:item]][:clearance]
+         }
+       end
+    cart[coupon[:item]][:count] -= coupon[:num]
+   end
+ end
 end
 
 
